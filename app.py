2d483f6b8d74f0f97ce93a891e0f237ed1e71900
bogdanluca14 <bogdanlucaalexandru@gmail.com>
2025-07-24 18:00:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -115,10 +115,7 @@ else:
         # Logout
         authenticator.logout("Logout", "sidebar")
 
-    elif auth_status is False:
-        st.error("Username sau parola incorectă.")
-    else:
-        st.warning("Te rog loghează-te.")
-        st.error("Username sau parola incorectă.")
-    else:
-        st.warning("Te rog loghează-te.")
+            elif auth_status is False:
+            st.error("Username sau parola incorectă.")
+        else:
+            st.warning("Te rog loghează-te.")
